a860a3af688451a7b111b42729f9609a3d5db2bd
刘清硕(轻字节编程，Light Byte Code) <111341725+LightByteCode@users.noreply.github.com>
2022-12-11 21:31:12 +0800

    Add files via upload

--- main.pyw
+++ main.pyw
@@ -1,16 +1,22 @@
-import wx,threading
+import wx,threading,module,pynput
 
 app=wx.App()
-
+#创建主窗口
 main_window=wx.Frame(None,title="LightScreenShot")
 main_window.Show()
-
+#设置主窗口
 main_window.SetIcon(wx.Icon('images/icon.jpg'))
 main_window.SetSize((800,600))
 main_window.Center()
-
-btn=wx.Button(main_window,label="Full",pos=(0,0))
+#实例化类
+ImageShot=module.ImageShot()
+#功能
+def full_screenshot(event):
+    ImageShot.full_screenshot()
+#按钮
+btn=wx.Button(main_window,label="✅",pos=(0,0))
 btn.Show()
-btn.Bind()
+
+btn.Bind(wx.EVT_BUTTON,full_screenshot)
 
 app.MainLoop()
--- module.py
+++ module.py
@@ -1,12 +1,45 @@
 from PIL import Image,ImageGrab
-import json
+import json,os,datetime,wx,time
 
 with open("settings.json","r",encoding="utf-8") as f:
     settings=json.load(f)
+    
+def create_dirs(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+alpha=200
+def success_msgbox(image,mode):
+    '''
+        image:PIL Image type;
+        mode:"image","gif","video"
+    '''
+    global alpha
+
+    screen_size=wx.DisplaySize()
+    msg=wx.Frame(None,size=(200,100),pos=(screen_size[0]-200,screen_size[1]-100),style=wx.FRAME_NO_TASKBAR|wx.STAY_ON_TOP)
+    msg.Show()
+
+    text=wx.StaticText(msg,label="截图成功")
+    text.Show()
+
+    picture=wx.Image(image,wx.BITMAP_TYPE_ANY)
+    picture.Scale(100,100)
+    picture=wx.StaticBitmap(msg,-1,wx.BitmapFromImage(picture))
+    picture.Show()
+
+    time.sleep(2)
+    msg.Destroy()
 
 class ImageShot:
-    def full_screenshot(self,path,mode=settings["image-save-mode"]):
+    def __init__(self):
+        pass
+    def full_screenshot(self,mode=settings["image-save-mode"]):
         image=ImageGrab.grab()
         if mode=="file-save":
-            image.save(settings["save-path"]+"//images")
-      
+            create_dirs(settings["image-save-path"])
+            image.save(settings["image-save-path"]+datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")+".jpg")
+
+            success_msgbox(settings["image-save-path"]+datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")+".jpg","image")
+
+    def rect_screenshot(self,path,mode=settings["image-save-mode"]):
+        pass
