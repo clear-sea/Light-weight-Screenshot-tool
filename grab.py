f8b42db3b6594823b612ab0edd2e3fb092e0ffb6
轻字节编程(Light Byte Code) <111341725+LightByteCode@users.noreply.github.com>
2023-01-29 11:21:25 +0800

    Add files via upload

--- grab.py
+++ grab.py
@@ -1,84 +1,45 @@
-from PIL import Image,ImageGrab
-import json,os,datetime,wx
+import time
+from PIL import Image,ImageGrab,ImageTk
+import json
+import os
+import datetime
+import tkinter as tk
+import threading
 
-#主窗口类
-class Window(wx.Frame):
-    def __init__(self,*args,**kw):
-        super().__init__(*args,**kw)
-        #初始化
-        self.p1=wx.Panel(self,pos=(0,0),size=(400,30))
-        self.p1.Show()
-        self.p2=wx.Panel(self,pos=(0,30),size=(400,100))
-        self.p2.Show()
-        self.p2.SetBackgroundColour(wx.WHITE)
-
-        self.width,self.height=400,130
-
-        self.SetSize(self.width,self.height)
-
-        self.title=wx.StaticText(self.p1,label="LightScreenShot",pos=(30,0),size=(self.width-60,30))
-        self.title.Show()
-        self.title.SetFont(wx.Font(20,wx.DEFAULT,wx.NORMAL,wx.NORMAL))
-        self.title.SetBackgroundColour(wx.WHITE)
-
-        self.icon=wx.StaticBitmap(self.p1,bitmap=wx.Image("images/icon.jpg").ConvertToBitmap(),pos=(0,0))
-        self.icon.Show()
-
-        self.close_btn=wx.StaticBitmap(self.p1,bitmap=wx.Image("images/close0.png").ConvertToBitmap(),pos=(self.width-30,0))
-        self.close_btn.Show()
-        #绑定事件
-        self.close_btn.Bind(wx.EVT_LEFT_DOWN,self.Windowsclose)
-
-        for widget in [self,self.p1,self.p2,self.icon,self.title]:
-            widget.Bind(wx.EVT_LEFT_DOWN, self.OnPanelLeftDown)
-            widget.Bind(wx.EVT_MOTION, self.OnPanelMotion)
-            widget.Bind(wx.EVT_LEFT_UP, self.OnPanelLeftUp)
-        
-    def OnPanelLeftDown(self, event):
-        pos = event.GetPosition()
-        x, y = self.ClientToScreen(event.GetPosition())
-        ox, oy = self.GetPosition()
-        dx = x - ox
-        dy = y - oy
-        self.delta = ((dx, dy))
- 
-    def OnPanelMotion(self, event):
-        if event.Dragging() and event.LeftIsDown():
-            mouse=wx.GetMousePosition()
-            self.Move((mouse.x-self.delta[0],mouse.y-self.delta[1]))
- 
-    def OnPanelLeftUp(self, event):
-        if self.HasCapture():
-            self.ReleaseMouse()
-
-    def Windowsclose(self,event):
-        self.Close()#⚠️后期改为隐藏窗口
-
-
+#导入之后执行的任务
+#读取配置文件
 with open("settings.json","r",encoding="utf-8") as f:
     settings=json.load(f)
+#读取所需的图片文件
+start_end_img=Image.open("images/start_end.png")
+close_img=Image.open("images/close.png")
     
 def create_dirs(path):
     if not os.path.exists(path):
         os.mkdir(path)
 
-def success_msg(image,mode,file_path):
+def success_msg(main_window,mode,file_path):
     '''
         image:PIL Image type;
         mode:"image","gif","video"
     '''
-    screen_size=wx.DisplaySize()
-    msg=wx.Frame(None,size=(230,100),pos=(screen_size[0]-200,screen_size[1]-100),style=wx.FRAME_NO_TASKBAR|wx.STAY_ON_TOP)
-    msg.Show()
-    msg.SetTransparent(150)
+    alpha=1.0
+    msg=tk.Toplevel(main_window)
+    
+    screen_size=(msg.winfo_screenwidth(),msg.winfo_screenheight())
+    msg.geometry(f"250x50+{screen_size[0]-250}+{screen_size[1]-50}")
+
+    msg.attributes("-alpha",1.0)
+    msg.attributes("-topmost",True)
+    msg.overrideredirect(True)
 
     def close(event):
-        msg.Close()
+        msg.destroy()
+
     def open_image(event):
         Image.open(file_path).show()
 
-
-    msg.Bind(wx.EVT_LEFT_DOWN,close)
+    msg.bind("<Button-1>",close)
 
     if mode=="image":
         info="截图成功(左键单击关闭)"
@@ -89,75 +50,142 @@ def success_msg(image,mode,file_path):
     else:
         info=None
 
-    t0=wx.StaticText(msg,label=info,pos=(0,0))
-    t1=wx.StaticText(msg,label=f"已保存在:\n{file_path}\n点击打开",pos=(0,20))
-    t1.SetFont(wx.Font(8,wx.DEFAULT,wx.NORMAL,wx.NORMAL,underline=True))
-    t1.Bind(wx.EVT_LEFT_DOWN,open_image)
-    t1.Show()
-    t0.Show()
+    t0=tk.Label(msg,text=info,font=("宋体",10))
+    t0.place(x=0,y=0,width=200,height=25)
+    t1=tk.Label(msg,text=f"已保存在:\n{file_path}\n点击打开",font=("宋体",7,"underline"))
+    t1.place(x=0,y=25,width=200,height=25)
+    t2=tk.Label(msg,image=ImageTk.PhotoImage(Image.open(file_path).resize((50,50))))
+    t2.place(x=200,y=0,width=50,height=50)
 
-def save(image,mode=settings["image-save-mode"]):
+    t1.bind("<Button-1>",open_image)
+
+def save(main_window,image,mode=settings["image-save-mode"]):
     if mode=="file-save":
         create_dirs(settings["image-save-path"])
         file_path=settings["image-save-path"]+datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")+".jpg"
         image.save(file_path)
         
-        success_msg(image,"image",file_path)
+        success_msg(main_window,"image",file_path)
 
 class ScreenShot:
+    def __init__(self):
+        self.image=None 
+        self.start_x=self.start_y=self.end_x=self.end_y=0 
+        self.frames=[]
+        self.GIF_thread=threading.Thread(target=self.GIF_begin)
+
     def full_screenshot(self,main_window):
-        main_window.Show(False)
+        main_window.attributes("-alpha",0)
         self.image=ImageGrab.grab()
-        main_window.Show(True)
-        return self.image
+        main_window.attributes("-alpha",1)
+        save(main_window,self.image)
 
     def rect_screenshot(self,main_window):
-        main_window.Show(False)
+        main_window.attributes("-alpha",0)#主窗口隐藏
+        self.image=ImageGrab.grab()#先截取全屏幕
         #初始化
-        fsc_window=wx.Frame(main_window,style=wx.STAY_ON_TOP)
-        fsc_window.ShowFullScreen(True)
-        fsc_window.SetTransparent(100)
-        dc=wx.ClientDC(fsc_window)
+        fsc_window=tk.Toplevel(main_window)
+        fsc_window.attributes("-alpha",0.6)
+        fsc_window.attributes("-topmost",True)
+        fsc_window.attributes("-fullscreen",True)
 
-        self.start_x=0
-        self.start_y=0
-        self.end_x=0
-        self.end_y=0
+        cv=tk.Canvas(fsc_window,bg="white")
+        cv.place(x=0,y=0,width=fsc_window.winfo_screenwidth(),height=fsc_window.winfo_screenheight())
 
         def grab(event):
-            main_window.Show(False)
-            self.image=ImageGrab.grab()
-            main_window.Show(True)
-            fsc_window.Close()
+            if not((self.end_x-self.start_x==0)and(self.end_y-self.start_y==0)):
+                if not((self.end_x<self.start_x)or(self.end_y<self.start_y)):
+                    self.image=self.image.crop((self.start_x,self.start_y,self.end_x,self.end_y))#再把全屏截图截取指定部分
+                    fsc_window.destroy()
+                    main_window.attributes("-alpha",1)   
+                    save(main_window,self.image)
+
+            self.start_x=self.start_y=self.end_x=self.end_y=0
 
         def start(event):
-            pos=event.GetPosition()
-            self.start_x,self.start_y=pos.x,pos.y
+            self.start_x,self.start_y=event.x,event.y
 
         def OnDrag(event):
-            dc.Clear()
-            pos=event.GetPosition()
-            dc.DrawRectangle(self.start_x,self.start_y,self.end_x-self.start_x,self.end_y-self.start_y)
+            self.end_x=event.x
+            self.end_y=event.y
+            cv.delete("all")
+            cv.create_rectangle(self.start_x,self.start_y,self.end_x-self.start_x,self.end_y-self.start_y,fill="black")
 
         def close(event):
-            if event.GetKeyCode()==wx.WXK_ESCAPE:
-                fsc_window.Close()
+            fsc_window.destroy()
 
-        fsc_window.Bind(wx.EVT_RIGHT_DOWN,grab)
-        fsc_window.Bind(wx.EVT_LEFT_DOWN,start)
-        fsc_window.Bind(wx.EVT_MOTION,OnDrag)
-        fsc_window.Bind(wx.EVT_KEY_DOWN,close)
+        fsc_window.bind("<ButtonRelease-1>",grab)
+        fsc_window.bind("<Button-1>",start)
+        fsc_window.bind("<B1-Motion>",OnDrag)
+        fsc_window.bind("<Escape>",close)
 
-        main_window.Show(True)
+    def GIF_begin(self):
+        while True:
+            self.frames.append(ImageGrab.grab((self.start_x,self.start_y,self.end_x,self.end_y)))
+            time.sleep(1/24)
 
-        return self.image
+    def GIF_screenshot(self,main_window,mode=settings["gif-save-mode"]):
+        #初始化
+        fsc_window=tk.Toplevel(main_window)
+        fsc_window.attributes("-alpha",0.6)
+        fsc_window.attributes("-topmost",True)
+        fsc_window.attributes("-fullscreen",True)
 
-    def GIF_begin(self,path,mode=settings["gif-save-mode"]):
-        image=ImageGrab.grab()
-        
+        cv=tk.Canvas(fsc_window,bg="white")
+        cv.place(x=0,y=0,width=fsc_window.winfo_screenwidth(),height=fsc_window.winfo_screenheight())
 
-    def GIF_end(self):
-        pass
+        def start(event):
+            #结束划定区域，开始录GIF
+            is_doing=tk.BooleanVar(main_window,True)
+            fsc_window.destroy()
+            #创建并且初始化工具条窗口
+            tool_bar=tk.Toplevel(main_window)
+            tool_bar.geometry(f"100x25+{tool_bar.winfo_screenwidth()-100}+{int(tool_bar.winfo_screenheight()/2)}")
+            tool_bar.attributes("-topmost",True)
+            tool_bar.overrideredirect(True)
+
+            def stop_GIF():
+                threading.Thread._Thread__stop(self.GIF_thread)
+                
+                file_path=settings["image-save-path"]+datetime.datetime.now().strftime("%Y-%m-%d_%H_%M_%S")+".gif"
+                self.frames[0].save(file_path,append_images=self.frames[1:],save_all=True,loop=True)
+                success_msg(main_window,"gif",file_path)
+            
+            b1=tk.Button(tool_bar,text="stop",command=stop_GIF)
+            b2=tk.Button(tool_bar,text="exit",command=lambda:tool_bar.destroy())
+            b1.place(x=0,y=0,width=50,height=25)
+            b2.place(x=50,y=0,width=50,height=25)
+            #开始录屏GIF
+            if not((self.end_x-self.start_x==0)and(self.end_y-self.start_y==0)):
+                if self.end_x<self.start_x:
+                    self.start_x,self.end_x=self.end_x,self.start_x
+                elif self.end_y<self.start_y:
+                    self.start_y,self.end_y=self.end_y,self.start_y
+
+                self.GIF_thread.start()
+                self.GIF_thread.join()
+
+            self.start_x=self.start_y=self.end_x=self.end_y=0
+
+        def press(event):
+            #按下左键开始划定区域
+            self.start_x,self.start_y=event.x,event.y
+
+        def OnDrag(event):
+            #正在拖拽
+            self.end_x=event.x
+            self.end_y=event.y
+            cv.delete("all")
+            cv.create_rectangle(self.start_x,self.start_y,self.end_x-self.start_x,self.end_y-self.start_y,fill="black")
+
+        def close(event):
+            #关闭全屏窗口
+            fsc_window.destroy()
+
+        fsc_window.bind("<ButtonRelease-1>",start)
+        fsc_window.bind("<Button-1>",press)
+        fsc_window.bind("<B1-Motion>",OnDrag)
+        fsc_window.bind("<Escape>",close)
 
     def video_begin(self):
         pass
--- main.pyw
+++ main.pyw
@@ -1,37 +1,34 @@
-import wx,threading,grab,pynput
+import threading,grab,pynput
+import tkinter as tk
 
-app=wx.App()
 #创建主窗口
 #主窗口初始化
-main_window=grab.Window(None,size=(400,100),style=wx.SIMPLE_BORDER)
-main_window.Center()
-main_window.Show()
+main_window=tk.Tk()
+main_window.geometry("400x100")
+main_window.iconbitmap("images/icon.ico")
+main_window.resizable(False,False)
+main_window.title("LightScreenShot")
+main_window["bg"]="white"
 #创建截图功能类
 screenshot=grab.ScreenShot()
 #功能
-def full_screenshot(event):
-    image=screenshot.full_screenshot(main_window)
-    grab.save(image)
+def full_screenshot():
+    screenshot.full_screenshot(main_window)
 
-def rect_screenshot(event):
-    image=screenshot.rect_screenshot(main_window)
-    grab.save(image)
+def rect_screenshot():
+    screenshot.rect_screenshot(main_window)
 
-#按钮
-#0
-font=wx.Font(15,wx.DEFAULT,wx.NORMAL,wx.NORMAL)
-btn1=wx.Button(main_window.p2,label="全屏截图",pos=(0,0),size=(100,100))
-btn1.SetFont(font)
-btn1.Show()
-btn1.SetCursor(wx.Cursor(wx.CURSOR_HAND))
+def GIF_screenshot():
+    screenshot.GIF_screenshot(main_window)
 
-btn1.Bind(wx.EVT_BUTTON,full_screenshot)
-#1
-btn2=wx.Button(main_window.p2,label="",pos=(100,0),size=(100,100))
-btn2.SetFont(font)
-btn2.Show()
-btn2.SetCursor(wx.Cursor(wx.CURSOR_HAND))
-btn2.Bind(wx.EVT_BUTTON,rect_screenshot)
+#全屏
+btn1=tk.Button(main_window,bg="white",text="全屏截图",cursor="hand2",command=full_screenshot)
+btn1.place(x=0,y=0,width=100,height=100)
+#矩形
+btn2=tk.Button(main_window,bg="white",text="矩形截图",cursor="hand2",command=rect_screenshot)
+btn2.place(x=100,y=0,width=100,height=100)
+#GIF
+btn3=tk.Button(main_window,bg="white",text="GIF截图",cursor="hand2",command=GIF_screenshot)
+btn3.place(x=200,y=0,width=100,height=100)
 
-
-app.MainLoop()
+main_window.mainloop()
